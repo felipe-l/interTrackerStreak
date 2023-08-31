ddcc9fbac91e0145a036c760aee3cc3edbc735fa
felipe-l <pipecarnal123@gmail.com>
2023-08-31 15:10:57 -0500

    Added summoners in config example

--- config.example.py
+++ config.example.py
@@ -1,2 +1,7 @@
 RIOT_KEY = 'your_riot_api_key_here'
 DISCORD_KEY = 'your_discord_bot_token_here'
+PLAYERS_TO_TRACK = [
+    'summoner_name1',
+    'summoner_name2',
+    # Add more summoner names here
+]
